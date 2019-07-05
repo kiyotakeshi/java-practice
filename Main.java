64fd1c88672d6d0e825ee9b20f3c9b1d7e8a2905
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2019-07-05 23:25:51 +0900

    add ignore

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class Main {
-    public static void main(String[] args) {
-        int r = new java.util.Random().nextInt(90);
-        System.out.println("乱数を発生させます: " + r);
-        // 乱数を発生させます: 53
-    }
-}
